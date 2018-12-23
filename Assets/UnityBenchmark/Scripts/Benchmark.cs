08e62472375521cd472a214f555577e4c338ed32
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-12-23 22:14:35 +0900

    Add Features.

---
+++ Assets/UnityBenchmark/Scripts/Benchmark.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace UnityBenchmark
+{
+    public class Benchmark
+    {
+        /// <summary>
+        /// <para>引数のActionの中で行われる処理にかかる時間をms単位で計測し、Logに出力する</para>
+        /// <para>【第1引数】実行したい処理</para>
+        /// </summary>
+        public static TimeSpan Execute(Action process)
+        {
+            return Execute(1, process);
+        }
+
+        /// <summary>
+        /// <para>引数のActionの中で行われる処理にかかる時間をms単位で計測し、Logに出力する</para>
+        /// <para>【第1引数】実行したい処理</para>
+        /// </summary>
+        public static TimeSpan Execute(int times, Action process)
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            for(int i = 0;i < times; ++i)
+            {
+                process();
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            Debug.Log(string.Format("{0} ms", ts.TotalMilliseconds));
+            return ts;
+        }
+
+        /// <summary>
+        /// <para>引数のActionの中で行われる処理にかかる時間をms単位で計測し、Logに出力する(Corutineで行う場合)</para>
+        /// <para>【第1引数】実行したい処理(コルーチンの処理yield returnを書いて実行する)</para>
+        /// </summary>
+        public static BenchmarkBehaviour ExecuteCoroutine(Func<IEnumerator> process)
+        {
+            return ExecuteCoroutine(1, process);
+        }
+
+        /// <summary>
+        /// <para>引数のActionの中で行われる処理にかかる時間をms単位で計測し、Logに出力する(Corutineで行う場合)</para>
+        /// <para>【第1引数】実行したい処理(コルーチンの処理yield returnを書いて実行する)</para>
+        /// </summary>
+        public static BenchmarkBehaviour ExecuteCoroutine(int times, Func<IEnumerator> process)
+        {
+            GameObject benchMarkObject = new GameObject();
+            BenchmarkBehaviour benchmarkBehaviour = benchMarkObject.AddComponent<BenchmarkBehaviour>();
+            benchmarkBehaviour.Execute(times, process);
+            return benchmarkBehaviour;
+        }
+    }
+}
---
+++ Assets/UnityBenchmark/Scripts/BenchmarkBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace UnityBenchmark
+{
+    public class BenchmarkBehaviour : MonoBehaviour
+    {
+        internal void Execute(int times, Func<IEnumerator> process)
+        {
+            StartCoroutine(ExecuteCoroutine(times, process));
+        }
+
+        private IEnumerator ExecuteCoroutine(int times, Func<IEnumerator> process)
+        {
+            System.Diagnostics.Stopwatch totalStopWatch = new System.Diagnostics.Stopwatch();
+            TimeSpan totalTime = new TimeSpan();
+            for (int i = 0; i < times; ++i)
+            {
+                System.Diagnostics.Stopwatch stepStopWatch = new System.Diagnostics.Stopwatch();
+                TimeSpan stepTime = new TimeSpan();
+                totalStopWatch.Start();
+                IEnumerator ie = process();
+                while (ie.MoveNext())
+                {
+                    stepStopWatch.Start();
+                    yield return ie.Current;
+                    stepStopWatch.Stop();
+                    TimeSpan onestepTime = stepStopWatch.Elapsed - stepTime;
+                    stepTime += onestepTime;
+                    totalStopWatch.Stop();
+                    Debug.Log(string.Format("onesteptime: {0} ms", stepTime.TotalMilliseconds));
+                    totalStopWatch.Start();
+                }
+                totalStopWatch.Stop();
+                TimeSpan currentTime = totalStopWatch.Elapsed - totalTime;
+                totalTime += currentTime;
+                Debug.Log(string.Format("onetime: {0} ms", currentTime.TotalMilliseconds));
+            }
+            Debug.Log(string.Format("total: {0} ms", totalTime.TotalMilliseconds));
+        }
+    }
+}
